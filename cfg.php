5433ac8c1f696d562f692c959288c59590d2242a
Prabu31 <53013360+Prabu31@users.noreply.github.com>
2021-10-21 22:07:56 +0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 5.1.1; A37f) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36";
+
+$cookie = "address_DOGE=DUMU6MBDKtZ7KdPTRnb9hLWW3WrvfencbR; dom3ic8zudi28v8lr6fgphwffqoz0j6c=35b2c897-988d-465f-8d2d-7bc2598e6466%3A3%3A1; PHPSESSID=8c4f09c75dbbef03e8418a5226ece10a";
+
+$doge = "DUMU6MBDKtZ7KdPTRnb9hLWW3WrvfencbR";
